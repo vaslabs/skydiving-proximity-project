e7e357fed3e3153dc97a0a53d0f1f44a641fc451
Vasilis Nicolaou <vaslabs@yahoo.gr>
2016-09-02 17:09:44 +0100

    First implementation

--- GPS_Test/GPS_Test.ino
+++ GPS_Test/GPS_Test.ino
@@ -1,86 +1,60 @@
 #include <TinyGPS.h>
-#include <DanyBotLCD.h>
-
-DanyBotLCD lcd(0x27, 16, 2);
  
 TinyGPS gps;
 int led = 13;
  
-long lat, lon;
+long lat, lon, alt;
 unsigned long fix_age, time, date, speed, course;
 unsigned long chars;
 unsigned short sentences, failed_checksum;
 //int year;
 //byte month, day, hour, minute, second, hundredths;
- 
+
+class GPSEntry {
+  public: 
+     long timestamp;
+     long latitude;
+     long longitude;
+     long altitude;
+};
+
+const int SIZE = 64;
+
+GPSEntry gps_entries[SIZE];
+int fifo_index = -1;
+
+
 int DEG;
 int MIN1;
 int MIN2;
-  
-void LAT(){                       //Latitude state
-  DEG=lat/1000000;
-  MIN1=(lat/10000)%100;
-  MIN2=lat%10000;
- 
-  lcd.setCursor(0,0);             // set the LCD cursor   position 
-  lcd.print("LAT:");              
-  lcd.print(DEG);
-  lcd.write(0xDF);
-  lcd.print(MIN1);
-  lcd.print(".");
-  lcd.print(MIN2);
-  lcd.print("'   ");
+
+void write_data() {
+     
 }
-void LON(){                        //Longitude state
-  DEG=lon/1000000;
-  MIN1=(lon/10000)%100;
-  MIN2=lon%10000;
- 
-  lcd.setCursor(0,1);              // set the LCD cursor   position 
-  lcd.print("LON:");              
-  lcd.print(DEG);
-  lcd.write(0xDF);
-  lcd.print(MIN1);
-  lcd.print(".");
-  lcd.print(MIN2);
-  lcd.print("'   ");
+
+void add(long timestamp, long lat, long lon, long alt) {
+    if (fifo_index == SIZE - 1) {
+      write_data();  
+      fifo_index = 0; 
+    }
+    else
+      fifo_index++;
+    gps_entries[fifo_index].timestamp = timestamp;
+    gps_entries[fifo_index].latitude = lat;
+    gps_entries[fifo_index].longitude = lon;
+    gps_entries[fifo_index].altitude = alt;
 }
- 
- 
  
 void setup()
 {
   Serial.begin(9600);            //Set the GPS baud rate.
   
-  pinMode(led, OUTPUT);  
-  lcd.init();  //Initializing the LCD 
-  lcd.backlight();
-  lcd.begin(16, 2);               // start the library
-  lcd.setCursor(0,0);             // set the LCD cursor   position 
-  lcd.print("GPS test");          // print a simple message on the LCD 
-  delay(2000);
 }
  
 void loop()
 {
-  while (Serial.available())
-  {
-    digitalWrite(led, HIGH);
-    int c = Serial.read();                   // Read the GPS data
-    if (gps.encode(c))                        // Check the GPS data
-    {
-      // process new gps info here
-    }
- 
-  }
-  digitalWrite(led, LOW);
   gps.get_position(&lat, &lon, &fix_age);     // retrieves +/- lat/long in 100000ths of a degree
+  alt = gps.altitude();
   gps.get_datetime(&date, &time, &fix_age);   // time in hhmmsscc, date in ddmmyy
- 
-  //gps.crack_datetime(&year, &month, &day,    //Date/time cracking
-  //&hour, &minute, &second, &hundredths, &fix_age);  
- 
-LAT();
-LON();
- 
+  add(time, lat, lon, alt);
 }
